0cceef1bbe88987678330609c88a0fb55c2b6597
AlexVDW <al.werff@gmail.com>
2019-02-14 21:14:32 +1100

    Initial Commit

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+const prefix = "!"
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(config.prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(config.prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+ 
+client.login(config.token);
