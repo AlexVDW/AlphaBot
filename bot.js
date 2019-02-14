59d12d40584b64d044ab1d403dd5c16f55e1e214
Alex <al.werff@gmail.com>
2019-02-15 09:50:36 +1100

    Added version number (0.1), about message

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const version = "0.1"
  
 client.on("ready", () => {
   console.log("I am ready!");
@@ -10,7 +11,10 @@ const prefix = "!"
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
- 
+  
+  if (message.content.startsWith(config.prefix + "about")) {
+    message.channel.send("Version "+ version + "\nhttps://github.com/AlexVDW/AlphaBot");
+  } else
   if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
   } else
