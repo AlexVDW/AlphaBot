b01e46b8a99a546db6b5c22f1d36c0cafac60f6d
Alex <al.werff@gmail.com>
2019-02-15 22:29:14 +1100

    added minimal scheduling; bot now can post date in #shedule using node-schedule

--- bot.js
+++ bot.js
@@ -1,10 +1,33 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+var schedule = require("node-schedule");
 const version = "0.3"
+
+//channels
+const ch_schedule = "545823492583718932";
+
+//variables
+const wd = [ 'Su','Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+const sch_len = 10;
+var d = new Date();
+
  
 client.on("ready", () => {
   console.log("I am ready!\nPrefix: \"" + config.prefix + "\"");
+  
+/*
+  const abc = client.channels.get(ch_schedule).fetchMessages({limit : 10})
+  .then(function(a){
+
+    //produce expected log
+    expected = wd.concat(wd).concat(wd).slice(d.getDay(),d.getDay()+sch_len)
+                 .map((a, i) => a + ' ' + d.getDate() + '/' + [d.getMonth()+1]);
+    console.log(expected);
+    
+    console.log(a.filter(m => m.author.id === '545560466420662274').map(g => g.content).reverse());
+  });
+*/
 });
  
 client.on("message", (message) => {
@@ -25,6 +48,15 @@ client.on("message", (message) => {
   } else {
     message.channel.send("command `" + command + "` not recognised")
   }
+});
+
+var j = schedule.scheduleJob('0 0 1 * * *', function(){
+  //Triggers 1am Melbourne time every day
+
+  //post new day (10 days in advance) (test will be a b c d e every minute)
+  client.channels.get(ch_schedule).send("wd[(d.getDay()+10)%7")
+    
+  //Delete old day
 });
  
 client.login(config.token);
