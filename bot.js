504bd78caab64d93a9a6d523dbfa3a16d2df1739
Alex <al.werff@gmail.com>
2019-02-15 12:43:16 +1100

    added command handling, fallback for unknown command

--- bot.js
+++ bot.js
@@ -11,14 +11,19 @@ client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
   
-  if (message.content.startsWith(config.prefix + "about")) {
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'about') {
     message.channel.send("Version "+ version + "\nhttps://github.com/AlexVDW/AlphaBot");
   } else
-  if (message.content.startsWith(config.prefix + "ping")) {
+  if (command === 'ping') {
     message.channel.send("pong!");
   } else
-  if (message.content.startsWith(config.prefix + "foo")) {
+  if (command === 'foo') {
     message.channel.send("bar!");
+  } else {
+    message.channel.send("command `" + command + "` not recognised")
   }
 });
  
